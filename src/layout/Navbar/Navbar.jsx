b0e2501007f086f08439b8f105c7fbaf2bc60f52
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-03-27 20:30:46 -0500

    navbar

--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -3,6 +3,18 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
+      <div>
+        <img src="" alt="" />
+        <h3>CINE COLOMBIA</h3>
+      </div>
+      <div>
+        <button>Acción</button>
+        <button>Terror</button>
+        <button>Ciencia Ficción</button>
+        <button>Comedia</button>
+      </div>
+      <div></div>
+      <div></div>
       
     </div>
   )
