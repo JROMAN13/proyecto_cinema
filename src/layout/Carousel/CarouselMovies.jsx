a221c250b551a6a927899302d901fa23b8cb8cc8
Jerson Roman <jroman1394@gmail.com>
2024-03-28 17:55:37 -0500

    Se realizan estilos sobre el carousel

--- src/layout/Carousel/CarouselMovies.jsx
+++ src/layout/Carousel/CarouselMovies.jsx
@@ -3,53 +3,94 @@ import { getAllMovies } from '../../services/movieServices';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { left } from '@popperjs/core';
+
+
+function SampleNextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "transparent", marginRight: "30px", zIndex: 1, height: "100%", color: "transparent", opacity: "0" }}
+            onClick={onClick}
+        />
+    );
+}
+
+function SamplePrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: "transparent", marginLeft: "30px", zIndex: 1, height: "100%", color: "transparent", opacity: "0" }}
+            onClick={onClick}
+        />
+    );
+}
+
+
 
 const CarouselMovies = () => {
-    const [movies,setMovies]=useState([]);
+
+    function handleClick(idImage) {
+
+        const imageSelected= document.getElementById(idImage);
+        console.log(imageSelected);     
+    }
+    
+    const [movies, setMovies] = useState([]);
     const urlBaseImage = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
-    useEffect(()=>{
+    useEffect(() => {
         getAllMovies().then(
-           (response)=>{
-            console.log(response)
-            setMovies(response)
-           }
+            (response) => {
+                console.log(response)
+                setMovies(response)
+            }
         ).catch(
-            (error)=> console.error(error)
+            (error) => console.error(error)
         )
-    },[])
+    }, [])
     const settings = {
         dots: true,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1
-      };
-  return (
-    <div className='w-3/4 m-auto bg-black'>
-        <div className='mt-20'>
-        <Slider {...settings}>
-            {
-                movies.map(
-                    (movie,index)=>(
-                        // console.log(`${urlBaseImage}${movie.poster_path}`)
-                        // console.log("movie",movie.poster_path)
-                        <>
-                            <div key={index} className='bg-white h-[450px] text-black rounder-xl'>
-                                <div className='h-56 rounder-t-xl bg-indigo-500 flex justify-center items-center'>
-                                <img src={`${urlBaseImage}${movie.poster_path}`} className='h-44 w-44 rouded full' />
-                                </div>
-                                <div></div>
-                            </div>
-                       
-                        </>
-                    )
-                )
-            }
-         </Slider>
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        nextArrow: <SampleNextArrow />,
+        prevArrow: <SamplePrevArrow />
+    };
+
+
+    return (
+        <div className='w-full bg-black'>
+            <div className='mt-20'>
+                <Slider {...settings}>
+                    {
+                        movies.map(
+                            (movie, index) => {
+                                // console.log(`${urlBaseImage}${movie.poster_path}`)
+                                // console.log("movie",movie.poster_path)
+
+
+                                const idImage = `image${index}`
+                                return (
+                                    <div key={index} className='bg-white h-[100%] text-black rounder-xl'>
+                                        <div className='h-90 rounder-t-xl  flex justify-center items-center' >
+                                            <img id={idImage} src={`${urlBaseImage}${movie.poster_path}`} onClick={() => handleClick(idImage)} className='w-full rouded full hover:cursor-pointer ' />
+                                        </div>
+                                        <div></div>
+                                    </div>
+
+                                );
+                            })
+                    }
+                </Slider>
+            </div>
+
         </div>
-      
-    </div>
-  )
+    )
 }
+
+
 
 export default CarouselMovies
