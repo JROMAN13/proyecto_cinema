271aa1f0528f421211d4645ce8524e8a66a9abc1
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-03-28 22:33:42 -0500

    imagen seleccionada ya se ve la clase active y la seccion addicional provisional

--- src/layout/Carousel/CarouselMovies.jsx
+++ src/layout/Carousel/CarouselMovies.jsx
@@ -27,18 +27,11 @@ function SamplePrevArrow(props) {
     );
 }
 
-
-
 const CarouselMovies = () => {
 
-    function handleClick(idImage) {
-
-        const imageSelected= document.getElementById(idImage);
-        imageSelected.classList.add("imgActive")
-        console.log("imagen seleccionada", imageSelected)
-    }
-    
     const [movies, setMovies] = useState([]);
+    const [showAdditionalSection, setShowAdditionalSection] = useState(false);
+    const [selectedImageIndex, setSelectedImageIndex] = useState(2);
     const urlBaseImage = "https://image.tmdb.org/t/p/w300_and_h450_bestv2";
     useEffect(() => {
         getAllMovies().then(
@@ -50,6 +43,14 @@ const CarouselMovies = () => {
             (error) => console.error(error)
         )
     }, [])
+
+    // function handleClick(idImage) {
+
+    //     const imageSelected= document.getElementById(idImage);
+    //     imageSelected.classList.add("imgActive")
+    //     console.log("imagen seleccionada", imageSelected)
+    // }
+
     const settings = {
         dots: true,
         infinite: true,
@@ -59,33 +60,48 @@ const CarouselMovies = () => {
         nextArrow: <SampleNextArrow />,
         prevArrow: <SamplePrevArrow />
     };
-
+    function handleClick(index) {
+        setSelectedImageIndex(index);
+        setShowAdditionalSection(true);
+    }
 
     return (
         <>
         <div className='w-full'>
             <div className='mt-20'>
                 <Slider {...settings}>
-                    {
+                    {/* {
                         movies.map(
                             (movie, index) => {
                                 // console.log(`${urlBaseImage}${movie.poster_path}`)
                                 // console.log("movie",movie.poster_path)
                                 const idImage = `image${index}`
                                 return (
-                                    <div id={idImage} key={index} className='bg-white h-[100%] text-black rounder-xl'>
-                                        <div className='h-90 rounder-t-xl  flex justify-center items-center' >
+                                    <div id={idImage} key={index} className='border border-blue-700 bg-white h-80 text-black rounder-xl' onClick={() => handleClick(idImage)}>
+                                        {/* <div className='h-80 border border-gray-text rounder-t-xl  flex justify-center items-center' >
                                             <img  src={`${urlBaseImage}${movie.poster_path}`} onClick={() => handleClick(idImage)} className='w-full rouded full hover:cursor-pointer ' />
-                                        </div>
-                                        <div></div>
+                                        </div> }
                                     </div>
 
                                 );
                             })
-                    }
+                    } */}
+                     {movies.map((movie, index) => {
+                        return (
+                            <div key={index} className={`image-container ${selectedImageIndex === index ? 'selected' : ''} rounded-lg`} onClick={() => handleClick(index)}>
+                                <img src={`${urlBaseImage}${movie.poster_path}`} alt={`Movie ${index}`} className='image rounded-lg ' />
+                                {selectedImageIndex === index && (
+                                    <div className='additional-section'>
+                                       <p className='text-red-500 font-semiblod'>movie</p>
+                                    </div>
+                                )}
+                            </div>
+
+                        );
+                    })}
                 </Slider>
             </div>
-
+            
         </div>
         </>
     )
