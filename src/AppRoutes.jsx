c1321df436cddacba0062fa2f595edc6086887dd
ibelabrador <ibelabrador@gmail.com>
2024-04-01 14:34:13 -0500

    formulario metodo de pago, inst FontAwesomeIcon

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Layout from './layout/Layout'
-import CardList from './components/CardList'
-import { Route, Routes } from 'react-router'
+import React from "react";
+import Layout from "./layout/Layout";
+import CardList from "./components/CardList";
+import PaymentMethod from './pages/payment/PaymentMethod';
+import { Route, Routes } from "react-router";
 
 const AppRoutes = () => {
   return (
     <>
       <Routes>
         <Route element={<Layout></Layout>}>
-          <Route index element={<CardList></CardList>}>
-          </Route>
+          <Route index element={<CardList></CardList>}></Route>
+          <Route path="/payment" element={<PaymentMethod></PaymentMethod>}></Route>
         </Route>
         {/* <Route index element={<Card></Card>}></Route> */}
       </Routes>
     </>
-  )
-}
+  );
+};
 
-export default AppRoutes
+export default AppRoutes;
