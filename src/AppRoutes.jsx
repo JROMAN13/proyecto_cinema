fd9d4fd8e4233091ba584990ce4ab9f2da0fc152
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-04-02 22:33:37 -0500

    se agrego rutas de details

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -14,7 +14,8 @@ const AppRoutes = () => {
           <Route index element={<CardList/>}></Route>
           <Route path='seats' element={<Seats/>}></Route>
           <Route path='s' element={<Asientos/>}></Route>
-          <Route path="details/:idPelicula" element={<DetailMovie/>}/>
+          {/* <Route path="details/:idPelicula" element={<DetailMovie/>}/>
+          <Route path="select_ticket" element={<DetailMovie/>}/> */}
         </Route>
         {/* <Route index element={<Card></Card>}></Route> */}
       </Routes>
