cbac2830f478212b4f9af02ed1b98b2fb2bd0733
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-04-03 21:37:57 -0500

    id cinema seleccionado enviado al local storage

--- src/layout/Navbar/SelectCinema.jsx
+++ src/layout/Navbar/SelectCinema.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { getAllCinemas } from '../../services/cinemaServices';
 
+
 const SelectCinema = () => {
     const [cinemas, setCinemas] = useState([])
     const [selectedCinema,setSelectedCinema]=useState("")
+
     useEffect(()=>{
        getAllCinemas().then((response)=>{
         // console.log(response);
@@ -11,24 +13,23 @@ const SelectCinema = () => {
         }).catch((error)=> console.error(error));
     },[])
 
-    // const getCinemaSelected =()=>{
-    //   try {
-    //     setSelectedCinema()
-    //   } catch (error) {
-    //     console.log(error)
-    //     return null
-    //   }
-    // }
+
+    const handleSelectChange = (event) => {
+      console.log(event.target.value);
+      setSelectedCinema(event.target.value);
+      localStorage.setItem("selectedCinemaId", event.target.value);
+      
+    };
 
   return (
     <form className="md:order-2">
-          <label htmlFor="countries" className="block mb-2 text-base font-semibold text-gray-text textSecundary ">Cines cercanos</label>
-          <select id="countries" className="bg-transparent border border-gray-text text-gray-text textSecundary text-sm rounded-lg focus:ring-blue-button focus:border-blue-button block w-full p-2.5">
+          <label htmlFor="cinemas" className="block mb-2 text-base font-semibold text-gray-text textSecundary ">Cines cercanos</label>
+          <select id="cinemas" className="bg-transparent border border-gray-text text-gray-text textSecundary text-sm rounded-lg focus:ring-blue-button focus:border-blue-button block w-full p-2.5" onChange={handleSelectChange} value={selectedCinema} >
             <option>Seleccione cine</option>
             {
                 cinemas.map((cinema,index)=>
                 // {console.log(cinema.name)}
-                <option key={index} value={cinema.name}>{cinema.name}</option>
+                <option key={index} value={cinema.id}>{cinema.name}</option>
                 )
             }
           </select>
@@ -37,3 +38,4 @@ const SelectCinema = () => {
 }
 
 export default SelectCinema
+
