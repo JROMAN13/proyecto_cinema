549708d576510611ce710b98f84054aa0f31d780
ibelabrador <ibelabrador@gmail.com>
2024-04-05 10:14:43 -0500

    merge payment-method y PurchaseSummary

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from './layout/Layout'
 import CardList from './components/CardList'
-import { Route, Routes } from 'react-router'
 import Seats from './components/Seats'
 import Asientos from './components/Carousel'
 import SelectTickets from './components/SelectTickets'
+import PaymentMethod from './pages/payment/PaymentMethod';
+import { Route, Routes } from 'react-router'
 
 const AppRoutes = () => {
   return (
@@ -15,6 +16,7 @@ const AppRoutes = () => {
           <Route path='seats' element={<Seats/>}></Route>
           <Route path='select_tickets' element={<SelectTickets/>}></Route>
           <Route path='s' element={<Asientos/>}></Route>
+          <Route path="/payment" element={<PaymentMethod></PaymentMethod>}></Route>
         </Route>
         {/* <Route index element={<Card></Card>}></Route> */}
       </Routes>
